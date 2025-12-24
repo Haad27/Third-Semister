094722aadc05684e3821213ef44a8413e3a91b3c
Haad majeed <haadmajeedch27@gmail.com>
2025-12-24 10:29:39 +0500

    sorting done as well lets do

---
+++ Theory/sort/all_sort.cpp
@@ -0,0 +1,122 @@
+
+#include <iostream>
+using namespace std;
+void bubble_sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+        }
+    }
+}
+
+void selection(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        swap(arr[i], arr[min]);
+    }
+}
+void insertion_Sort()
+{
+    int size = 5;
+    int arr[size] = {2, 6, 1, 5, 2};
+    int key;
+    for (int i = 1; i < size; i++)
+    {
+        key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int l[n1], r[n2];
+    for (int i = 0; i < n1; i++)
+    {
+        l[i] = arr[left + i];
+    }
+    for (int j = 0; j < n2; j++)
+    {
+        r[j] = arr[mid + 1 + j];
+    }
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2)
+    {
+        if (l[i] <= r[j])
+        {
+            arr[k++] = l[i++];
+        }
+        else if (l[i] >= r[j])
+        {
+            arr[k++] = r[j++];
+        }
+    }
+    while (i < n1)
+    {
+        arr[k++] = l[i++];
+    }
+    while (j < n2)
+    {
+        arr[k++] = r[j++];
+    }
+}
+void mergesort(int arr[], int left, int right)
+{
+
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        mergesort(arr, left, mid);
+        mergesort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+void quicksort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+        int pi = i + 1;
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
+int main()
+{
+
+    return 0;
+}
--- Theory/sort/bubble.cpp
+++ Theory/sort/bubble.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
-#include <string>
-#include <vector>
-
 using namespace std;
 
 int main()
 {
     int arr[5] = {2, 6, 1, 5, 2};
+    int n = 5;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = i+1 ; j < 5; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if (arr[i] > arr[j])
+            if (arr[j] > arr[j + 1])
             {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-    for (int i = 0; i < 5; i++)
+
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
